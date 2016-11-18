afc0f690bb76f6da2e75e18cc1c48a17cf47afc1
Varad Kishore <varad.kishore@gmail.com>
2016-11-17 22:55:14 -0500

    Insurance and location filters in explore

--- views/js/explore.js
+++ views/js/explore.js
@@ -1,3 +1,22 @@
+function GetURLParameter(sParam) {
+	var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+	return null;
+}
+
+var user_id = GetURLParameter('user_id');
+var user_profile;
+var care_type = GetURLParameter('care_type');
+if (care_type == null) care_type = 'medical';
+
 $(document).ready(function() {
 
 	$('.caretypetab').click(function(){
@@ -8,8 +27,6 @@ $(document).ready(function() {
 	var START_LAT = 40.741077;
 	var START_LONG = -74.002160;
 	var ZOOM = 15
-	// Used to remember markers
-	var markerStore = {};
 
 	var myLatlng = new google.maps.LatLng(START_LAT,START_LONG);
     var myOptions = {
@@ -19,28 +36,19 @@ $(document).ready(function() {
     }
     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
-	getMarkers();
+	markDoctors();
 
-	function getMarkers() {
-		console.log('getMarkers');
-		$.get('/markers', {}, function(res,resp) {
+	function markDoctors() {
+		console.log('markDoctors');
+		$.get('/find_doctors?user_id=1&care_type=medical&user_insurance=Aetna%20Group&lat=40.74107&long=-74.002160', {}, function(res,resp) {
 			console.dir(res);
 			for(var i=0, len=res.length; i<len; i++) {
-
-				//Do we have this marker already?
-				if(markerStore.hasOwnProperty(res[i].id)) {
-					markerStore[res[i].id].setPosition(new google.maps.LatLng(res[i].position.lat,res[i].position.long));
-				} else {
-					var marker = new google.maps.Marker({
-						position: new google.maps.LatLng(res[i].position.lat,res[i].position.long),
-						title:res[i].name,
-						map:map
-					});	
-					markerStore[res[i].id] = marker;
-					console.log(marker.getTitle());
-				}
+				var marker = new google.maps.Marker({
+					position: new google.maps.LatLng(res[i].position.lat,res[i].position.long),
+					label:res[i],
+					map:map
+				});
 			}
 		}, "json");
 	}
-	
-})
+});
