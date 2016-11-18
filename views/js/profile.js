1289386d907977989f7d2643f2c877c00edceaf5
QueueOverflow <Titan@dhcp-ccc-7481.eduroam.cornell.edu>
2016-11-18 12:48:10 -0500

    profile done

--- views/js/profile.js
+++ views/js/profile.js
@@ -15,36 +15,59 @@ function getURLParameter(sParam) {
 var user_id = getURLParameter('user_id');
 console.log(user_id);
 $(document).ready(function() {
-
+    // get insurance list from server
   $.ajax({
-
-                             url: "/get_insurances/",//problem with URL  + $("#username").val()+ $("#password").val(),
+                             url: "/get_insurances/",
                              type: "GET",
                              dataType : "json",
-
                              success: function( data ) {
-
-                             console.log("You received some data!", data);
+                            //  console.log("You received some data!", data);
                              for(var i = 0; i < data.length; i++){
                                 $('#insurancename select').append('<option>'+data[i]+'</option>');
                              }
-
-//                              if (data.username!=null)
-//                              {
-// // <!--                          $("#status").html("Successfully Logged In! You will be redirected soon");-->
-// //                              window.location.replace("userinformation.html");
-//
-//                              }
-//                              else {
-//
-//
-//                              }
                              },
 
               });
 
+    // get basic user information from server
+    $.ajax({
+                              url: "/get_user/",
+                              type: "GET",
+                              dataType : "json",
+                              data:{
+                                    user_id: user_id,
+                              },
+                              success: function( data ) {
+                              console.log("You received some data!", data.first_name);
+                                $('#firstname input').val(data.first_name);
+                                $('#lastname input').val(data.last_name);
+                                $('#emailaddress input').val(data.email_address);
+                                $('#insurancename select').val(data.insurance_name);
+                                $('#insurancenumber input').val(data.insurance_number);
+                                $('#groupnumber input').val(data.group_number);
+                              },
 
+            });
+      $("#save").click(function(){
+            $.ajax({
+                    url: "/update_user",
+                    type: "POST",
+                    data:{
+                        user_id: user_id,
+                        first_name: $('#firstname input').val(),
+                        last_name: $('#lastname input').val(),
+                        email_address: $('#emailaddress input').val(),
+                        insurance_name: $('#insurancename select').val(),
+                        insurance_number: $('#insurancenumber input').val(),
+                        group_number: $('#groupnumber input').val(),
+                    },
+                    success: function(){
+                        window.location = "explore.html?user_id=" + user_id;
+                    },
 
+            })
+
+      });
 
 
 });
